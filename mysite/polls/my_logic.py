77457517d2e3b224d9dd33a6cc5683a1aacd7c36
hako85hako <hoko85hako@gmail.com>
2022-05-04 19:56:37 +0900

    画面の合計値を算出するロジック作成

---
+++ mysite/polls/my_logic.py
@@ -0,0 +1,8 @@
+
+
+def total_payment(payment_object_list):
+    total_payment = 0
+    for payment_object in payment_object_list:
+        print(payment_object.payment_money)
+        total_payment += int(payment_object.payment_money)
+    return total_payment
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import T_PAYMENT
 from .models import M_CUSTOMER
+from polls import my_logic 
 
 def _index(request):
     latest_question_list = T_PAYMENT.objects
@@ -10,7 +11,9 @@ def _index(request):
 
 def index(request):
     payment_info_list = T_PAYMENT.objects.select_related('customer_id','payment_kind_id')
+    #取得したobjectの合計値を算出する
+    payment_total = my_logic.total_payment(payment_info_list)
 
     template = loader.get_template('polls/index.html')
-    context = {'payment_info_list': payment_info_list,}
+    context = {'payment_info_list': payment_info_list,'payment_total':payment_total}
     return HttpResponse(template.render(context, request))
